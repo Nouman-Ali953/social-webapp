1cecb8bdf9e5dfc51bc4c29b7538a95686961799
Nouman-Ali953 <nauman.9533@gmail.com>
2024-10-29 22:24:18 +0500

    try catch

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,10 +4,12 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import prisma from "@/lib/client";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
-  if (!WEBHOOK_SECRET) {
+  try {
+    
+    // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+    
+    if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
@@ -18,7 +20,7 @@ export async function POST(req: Request) {
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
-
+  
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
@@ -32,9 +34,9 @@ export async function POST(req: Request) {
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
-
+  
   let evt: WebhookEvent;
-
+  
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
@@ -48,14 +50,14 @@ export async function POST(req: Request) {
       status: 400,
     });
   }
-
+  
   // Do something with the payload
   // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
   // console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   // console.log('Webhook body:', body)
-
+  
   if (eventType === "user.created") {
     try {
       await prisma.user.create({
@@ -89,6 +91,9 @@ export async function POST(req: Request) {
       return new Response("Failed to update the user!", { status: 500 });
     }
   }
-
+  
   return new Response("Webhook received", { status: 200 });
+} catch (error) {
+  console.log(error)
+}
 }
